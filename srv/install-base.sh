eda0e275a58cc9698864f6bdd3e5bd09f1b6b610
Jeff Bowman <jeff.t.bowman@gmail.com>
2016-04-06 12:17:42 -0500

    rank mirrors for faster install begin adding code to create mule dev desktop

--- srv/install-base.sh
+++ srv/install-base.sh
@@ -31,6 +31,8 @@ echo "==> mounting ${ROOT_PARTITION} to ${TARGET_DIR}"
 /usr/bin/mount -o noatime,errors=remount-ro ${ROOT_PARTITION} ${TARGET_DIR}
 
 echo '==> bootstrapping the base installation'
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.save
+/usr/bin/rankmirrors -n 0 /etc/pacman.d/mirrorlist.save | /usr/bin/tee /etc/pacman.d/mirrorlist
 /usr/bin/pacstrap ${TARGET_DIR} base base-devel
 /usr/bin/arch-chroot ${TARGET_DIR} pacman -S --noconfirm gptfdisk openssh syslinux
 /usr/bin/arch-chroot ${TARGET_DIR} syslinux-install_update -i -a -m
@@ -44,31 +46,31 @@ echo '==> generating the system configuration script'
 /usr/bin/install --mode=0755 /dev/null "${TARGET_DIR}${CONFIG_SCRIPT}"
 
 cat <<-EOF > "${TARGET_DIR}${CONFIG_SCRIPT}"
-	echo '${FQDN}' > /etc/hostname
-	/usr/bin/ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
-	echo 'KEYMAP=${KEYMAP}' > /etc/vconsole.conf
-	/usr/bin/sed -i 's/#${LANGUAGE}/${LANGUAGE}/' /etc/locale.gen
-	/usr/bin/locale-gen
-	/usr/bin/mkinitcpio -p linux
-	/usr/bin/usermod --password ${PASSWORD} root
-	# https://wiki.archlinux.org/index.php/Network_Configuration#Device_names
-	/usr/bin/ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
-	/usr/bin/ln -s '/usr/lib/systemd/system/dhcpcd@.service' '/etc/systemd/system/multi-user.target.wants/dhcpcd@eth0.service'
-	/usr/bin/sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
-	/usr/bin/systemctl enable sshd.service
+        echo '${FQDN}' > /etc/hostname
+        /usr/bin/ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+        echo 'KEYMAP=${KEYMAP}' > /etc/vconsole.conf
+        /usr/bin/sed -i 's/#${LANGUAGE}/${LANGUAGE}/' /etc/locale.gen
+        /usr/bin/locale-gen
+        /usr/bin/mkinitcpio -p linux
+        /usr/bin/usermod --password ${PASSWORD} root
+        # https://wiki.archlinux.org/index.php/Network_Configuration#Device_names
+        /usr/bin/ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
+        /usr/bin/ln -s '/usr/lib/systemd/system/dhcpcd@.service' '/etc/systemd/system/multi-user.target.wants/dhcpcd@eth0.service'
+        /usr/bin/sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
+        /usr/bin/systemctl enable sshd.service
 
-	# Vagrant-specific configuration
-	/usr/bin/useradd --password ${PASSWORD} --comment 'Vagrant User' --create-home --gid users vagrant
-	echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_vagrant
-	echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_vagrant
-	/usr/bin/chmod 0440 /etc/sudoers.d/10_vagrant
-	/usr/bin/install --directory --owner=vagrant --group=users --mode=0700 /home/vagrant/.ssh
-	/usr/bin/curl --output /home/vagrant/.ssh/authorized_keys --location https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
-	/usr/bin/chown vagrant:users /home/vagrant/.ssh/authorized_keys
-	/usr/bin/chmod 0600 /home/vagrant/.ssh/authorized_keys
+        # Vagrant-specific configuration
+        /usr/bin/useradd --password ${PASSWORD} --comment 'Vagrant User' --create-home --gid users vagrant
+        echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_vagrant
+        echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_vagrant
+        /usr/bin/chmod 0440 /etc/sudoers.d/10_vagrant
+        /usr/bin/install --directory --owner=vagrant --group=users --mode=0700 /home/vagrant/.ssh
+        /usr/bin/curl --output /home/vagrant/.ssh/authorized_keys --location https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
+        /usr/bin/chown vagrant:users /home/vagrant/.ssh/authorized_keys
+        /usr/bin/chmod 0600 /home/vagrant/.ssh/authorized_keys
 
-	# clean up
-	/usr/bin/pacman -Rcns --noconfirm gptfdisk
+        # clean up
+        /usr/bin/pacman -Rcns --noconfirm gptfdisk
 EOF
 
 echo '==> entering chroot and configuring system'
